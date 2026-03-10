b757907dac31d4d90f1bef5650add536bfbcc20c
Mendez Vazquez <meluisa@emea.corpdir.net>
2026-03-10 16:15:09 +0100

    save changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,3 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-// https://astro.build/config
-export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
-});
+
+export default defineConfig({});
